05c85f007f8f529c4dd3e62ea5d0a5ae0f5693f7
Omar Essam <142716851+engomaressam@users.noreply.github.com>
2024-02-11 13:09:42 +0200

    Update deployment_app.py

--- deployment_app.py
+++ deployment_app.py
@@ -176,7 +176,16 @@ def display_project_type(selected_project):
 
 
 # %%
-if __name__ == '__main__':
-    app.run_server(debug=True)
+import os
+from threading import Timer
+import webbrowser
+
+def open_browser():
+    if not os.environ.get("WERKZEUG_RUN_MAIN"):
+        webbrowser.open_new('http://127.0.0.1:1222/')
+
+if __name__ == "__main__":
+    Timer(1, open_browser).start()
+    app.run_server(debug=True, port=1222)
 
 
